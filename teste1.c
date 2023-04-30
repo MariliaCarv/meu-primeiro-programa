e97d0642f71ff2ca6777f166b0d81641b0da6c18
Marilia Carvalho <102387236+gabimarilia@users.noreply.github.com>
2023-04-30 19:55:59 -0300

    Add files via upload

---
+++ teste1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include <locale.h>
+
+int main (void){
+    setlocale(LC_ALL, "portuguese");
+
+
+     printf("Listagem Dos Alunos\n ");
+     printf("Nome\t \t\tNota\n ");
+     printf("---------------------------\n");
+     printf("Ana Beatriz\t\t 8.5\n");
+         printf("Bianca Martins\t\t 9.0\n");
+             printf("Cláudio Sá\t\t 5.5\n");
+                 printf("Giovana Silva\t\t 7.5\n");
+
+
+          return 0;
+}
+
+ 
+  
+
+  
